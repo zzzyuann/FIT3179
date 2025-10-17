cde50267473333773bf9efa6877ad58eb9b0e632
zzzyuann <161961163+zzzyuann@users.noreply.github.com>
2025-10-17 12:31:40 +0800

    Delete DataVis2 directory

--- DataVis2/vega.js
+++
@@ -1,5 +0,0 @@
-var vg_1 = "week9_homework.vg.json";
-vegaEmbed("#netflix_map", vg_1).then(function(result) {
-// Access the Vega view instance
-//(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
